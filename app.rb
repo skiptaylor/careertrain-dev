e0df3eba4b5426112591809b6d47e216bd5ce603
Jarrod Taylor <jarrodtaylor@icloud.com>
2014-04-07 22:35:42 -0400

    Adds manual redirection
    I don't have this running to deploy from, but it should work.

--- app.rb
+++ app.rb
@@ -18,4 +18,15 @@ require_directory([
   'routes'
 ])
 
+# Try it now!
+configure :production do
+  before do
+    unless request.request_method == 'POST'
+      unless request.url.include? "https://www."
+        redirect "https://www.careertrain.com#{request.path}"
+      end
+    end
+  end
+end
+
 DataMapper.finalize
