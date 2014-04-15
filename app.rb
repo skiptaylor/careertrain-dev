9cb5939392ea87b88f1973a83232b658d092df0b
Skip Taylor <skiptaylor@clear.net>
2014-04-15 14:38:19 -0400

    Tries to fix sign in IE issue

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ require 'sinatra/chassis/helpers'
 
 require 'sinatra/reloader' if development?
 
+use Rack::Protection, :except => :session_hijacking
+
+enable :sessions
 set :session_secret, 'secret123'
 
 require_directory([
