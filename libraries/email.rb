25320bf05dcc572c13a166eec45628fe20148c86
Skip Taylor <tayloraid@gmail.com>
2015-03-16 13:29:45 -0400

    Adds me to email.

--- libraries/email.rb
+++ libraries/email.rb
@@ -19,14 +19,14 @@ class Email
     body << "<p>#{school_address1}</p>"
     body << "<p>#{school_address2}</p>"
     body << "<p>$#{school_city} #{school_state} #{school_zip}</p>"
-    body << "<p>Contact: #{first_name} #{last_name}</p>"
+    body << "<p>Contac: #{first_name} #{last_name}</p>"
     body << "<p>Email: #{email}</p>"
     body << "<p>Phone: #{phone}</p>"
     body << "<p>Fax: #{fax}</p>"
 
     Pony.mail(
       headers: { 'Content-Type' => 'text/html' },
-      to: 'info@careertrain.com',
+      to: 'info@careertrain.com, tayloraid@gmail.com',
       from: 'no-reply@careertrain.com',
       subject: 'Request CD2 Program from Educator',
       body: body
