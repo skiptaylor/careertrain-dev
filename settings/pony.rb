331eeee360f330835e1231bc871b58dbca35db11
Skip Taylor <skiptaylor101@gmail.com>
2020-09-18 11:26:37 -0400

    Adds pony SendGrid

--- settings/pony.rb
+++ settings/pony.rb
@@ -4,8 +4,8 @@ Pony.options = {
     address: 'smtp.sendgrid.net',
     port: '587',
     domain: 'heroku.com',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: 'eCDpony',
+    password: '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',
     authentication: :plain,
     enable_starttls_auto: true
   }
