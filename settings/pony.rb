49a96e74b67e824a674f458f50487d57cbbe6630
Skip Taylor <skiptaylor101@gmail.com>
2020-09-18 12:07:24 -0400

    Reverts username

--- settings/pony.rb
+++ settings/pony.rb
@@ -4,7 +4,7 @@ Pony.options = {
     address: 'smtp.sendgrid.net',
     port: '587',
     domain: 'heroku.com',
-    user_name: 'apikey',
+    user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     authentication: :plain,
     enable_starttls_auto: true
