03ef34f9310b8511a9d5ce2e16c1304e9be27baa
Skip Taylor <skiptaylor101@gmail.com>
2020-09-18 12:02:12 -0400

    api Key added

--- settings/pony.rb
+++ settings/pony.rb
@@ -4,7 +4,7 @@ Pony.options = {
     address: 'smtp.sendgrid.net',
     port: '587',
     domain: 'heroku.com',
-    user_name: ENV['SENDGRID_USERNAME'],
+    user_name: 'apikey',
     password: ENV['SENDGRID_PASSWORD'],
     authentication: :plain,
     enable_starttls_auto: true
