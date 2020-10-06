a6f5dab97404433119fafe38fc48be3ba60416f8
Skip Taylor <skiptaylor101@gmail.com>
2020-10-06 13:44:16 -0400

    adds scores to student

--- models/student.rb
+++ models/student.rb
@@ -32,15 +32,15 @@ class Student
   property :ex_score1, String
   property :ex_score2, String
   
-  property  :score_a,      Integer, default: 0
-  property  :score_b,      Integer, default: 0
-  property  :score_c,      Integer, default: 0
-  property  :score_d,      Integer, default: 0
-  property  :score_e,      Integer, default: 0
-  property  :score_f,      Integer, default: 0
-  property  :score_g,      Integer, default: 0
-  property  :score_h,      Integer, default: 0
-  property  :score_i,      Integer, default: 0
+  property  :score_a,  Integer, default: 0
+  property  :score_b,  Integer, default: 0
+  property  :score_c,  Integer, default: 0
+  property  :score_d,  Integer, default: 0
+  property  :score_e,  Integer, default: 0
+  property  :score_f,  Integer, default: 0
+  property  :score_g,  Integer, default: 0
+  property  :score_h,  Integer, default: 0
+  property  :score_i,  Integer, default: 0
 
   property :grade, String
   
