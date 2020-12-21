caa2d27c9689cc4d4a133a8dc0f6db00c0f115b0
Skip Taylor <skiptaylor101@gmail.com>
2020-12-21 11:52:55 -0500

    Adds arguments to email full report route

--- routes/student_reports.rb
+++ routes/student_reports.rb
@@ -248,7 +248,7 @@ get "/student/reports/:id/report/?" do
   @student = Student.get(params[:id])
   
   if @student.class_date == nil
-    Email.fullreport
+    Email.fullreport(to, first_name, last_name)
   
     @student.class_date = @student.created_on
     @student.save
