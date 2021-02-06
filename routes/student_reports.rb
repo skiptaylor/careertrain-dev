587103f61ebbdce1506caa29c1c937f26c0ac2b3
Skip Taylor <skiptaylor101@gmail.com>
2021-02-06 09:16:04 -0500

    fixes score3 == ''

--- routes/student_reports.rb
+++ routes/student_reports.rb
@@ -247,7 +247,7 @@ get "/student/reports/:id/report/?" do
   @presentation = Presentation.all
   @student = Student.get(params[:id])
   
- if @student.score3 == nil
+ if @student.score3 == ''
    Email.welcome(@student.email, @student.first_name, @student.last_name)
 
    @student.class_date = @student.created_on
