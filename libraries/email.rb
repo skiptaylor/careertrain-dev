f03497d3429a560262d4fed2a473bc9b48987629
Skip Taylor <skiptaylor101@gmail.com>
2021-05-08 10:59:19 -0400

    Changes student email "@"

--- libraries/email.rb
+++ libraries/email.rb
@@ -34,15 +34,15 @@ class Email
     )
   end
   
-  def self.welcome(email, first_name, last_name)
+  def self.welcome(student.email, student.first_name, student.last_name)
 
     body = ""
-    body << "<p>Hello #{first_name} #{last_name}. Welcometo eCareerDirecrion</p>"
+    body << "<p>Hello #{@student.first_name} #{@student.last_name}. Welcometo eCareerDirecrion</p>"
     body << "<p>Some words go here!</p>"
 
   Pony.mail(
     headers: { 'Content-Type' => 'text/html' },
-    to: "#{email}",
+    to: "#{@student.email}",
     from: 'no-rely@eCareerDirection.com',
     subject: 'Welcome to eCareerDirection.',
     body: body
