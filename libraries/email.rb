35385856e1eae8ac64e6edf732c8839b9c22d8af
Skip Taylor <skiptaylor101@gmail.com>
2022-05-12 09:20:33 -0400

    Updates email.rb

--- libraries/email.rb
+++ libraries/email.rb
@@ -38,7 +38,7 @@ class Email
 
   require "erb"
 
-  filename = "https://www.ecareerdirection.com/student/reports/#{id}/mail_wel"
+  filename = "https://www.ecareerdirection.com/student/reports/#{id}/mail_wel.erb"
 
   Pony.mail(
     to: "#{email}",
