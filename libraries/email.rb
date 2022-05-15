58dbdb2beb06e28fd6c8f0cce039f68d07a555d0
Skip Taylor <skiptaylor101@gmail.com>
2022-05-15 09:35:00 -0400

    final email style fix

--- libraries/email.rb
+++ libraries/email.rb
@@ -52,9 +52,9 @@ class Email
   def self.welcome(id, email, first_name, last_name, high1_show, high2_show)
 
     body = ""
-    body << "<p style='font-size: 20px;'>Hello #{first_name} #{last_name}. Welcome to <b><i>e</i>CareerDirecrion.</b></p>"
-    body << "<p>Your personalized interest report indicates your main interest is <b>#{high1_show}</b> and your next interest is <b>#{high2_show}.</b> You can return to your account any time to see your scores and watch videos or take the exercise a second time.<br /><br /></p>"
-    body = "<p>Thanks for your participation.<br />The <span style='font-weight: 900; letter-spacing: -1px;'><i>e</i>CareerDirection</span> Team</p>"
+    body << "<h3>Hello #{first_name} #{last_name}. Welcome to <b><i>e</i>CareerDirecrion.</b></h3>"
+    body << "<p style='font-size: 19px;'>Your personalized interest report indicates your main interest is <b>#{high1_show}</b> and your next interest is <b>#{high2_show}.</b> You can return to your account any time to see your scores and watch videos or take the exercise a second time.<br /><br /></p>"
+    body << "<p>Thanks for your participation.<br />The <span style='font-weight: 900; letter-spacing: -1px;'><i>e</i>CareerDirection</span> Team</p>"
     
   Pony.mail(
     headers: { 'Content-Type' => 'text/html' },
