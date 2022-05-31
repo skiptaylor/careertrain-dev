bcc87986de630b23e7729174462be95e85ffdd42
Skip Taylor <skiptaylor101@gmail.com>
2022-05-31 07:37:35 -0400

    Testing request email?

--- routes/educator.rb
+++ routes/educator.rb
@@ -10,23 +10,13 @@ end
 post '/educator/request/?' do
   state = State.all
   
-  body = ''
-  body << "<h3>Request eCD Program from Educator</h3>"
-  body << "<p><i>(School not yet active)</i></p>"
-  body << "<p><b>School Name:</b> #{params[:school_name]}</p>"
-  body << "<p>#{params[:school_address1]}</p>"
-  body << "<p>#{params[:school_address2]}</p>"
-  body << "<p>#{params[:school_city]} #{params[:state]} #{params[:school_zip]}</p>"
-  body << "<p><b>Contact:</b> <b>#{params[:first_name]} #{params[:last_name]}</p>"
-  body << "<p><b>Email:</b> <b>#{params[:email]}</p>"
-  body << "<p><b>Phone:</b> <b>#{params[:phone]}</p>"
 
   Pony.mail(
     headers: { 'Content-Type' => 'text/html' },
     to: 'skip@recountant.com',
     from: 'no-reply@eCareerDirection.com',
     subject: 'Request eCD Program from Educator',
-    body: body
+    body: "<p><b>School Name:</b> #{params[:school_name]}</p>"
   )  
   redirect "/educator/thanks"
 end
