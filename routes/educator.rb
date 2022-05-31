57926f5a8fd8803a2d464e7114a81f88b13689a1
Skip Taylor <skiptaylor101@gmail.com>
2022-05-31 08:15:21 -0400

    removes request validation

--- routes/educator.rb
+++ routes/educator.rb
@@ -10,13 +10,24 @@ end
 post '/educator/request/?' do
   state = State.all
   
+  body = ''
+  body << "<h3>Request eCD Program from Educator</h3>"
+  body << "<p><i>(School not yet active)</i></p>"
+  body << "<p><b>School Name:</b> #{params[:school_name]}</p>"
+  body << "<p>#{params[:school_address1]}</p>"
+  body << "<p>#{params[:school_address2]}</p>"
+  body << "<p>#{params[:school_city]} #{params[:state]} #{params[:school_zip]}</p>"
+  body << "<p><b>Contact:</b> <b>#{params[:first_name]} #{params[:last_name]}</p>"
+  body << "<p><b>Email:</b> <b>#{params[:email]}</p>"
+  body << "<p><b>Phone:</b> <b>#{params[:phone]}</p>"
+  body << "<p><b>Number of students:</b> <b>#{params[:students]}</p>"
 
   Pony.mail(
     headers: { 'Content-Type' => 'text/html' },
     to: 'skip@recountant.com',
     from: 'no-reply@eCareerDirection.com',
     subject: 'Request eCD Program from Educator',
-    body: "<p><b>School Name:</b> #{params[:school_name]}</p>"
+    body: body
   )  
   redirect "/educator/thanks"
 end
