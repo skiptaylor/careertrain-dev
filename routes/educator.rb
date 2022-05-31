f318b1068057d84cf754d5d97dcbea8814306351
Skip Taylor <skiptaylor101@gmail.com>
2022-05-31 09:57:17 -0400

    Finished request form

--- routes/educator.rb
+++ routes/educator.rb
@@ -11,20 +11,17 @@ post '/educator/request/?' do
   state = State.all
   
   body = ''
-  body << "<h3>Request eCD Program from Educator</h3>"
-  body << "<p><i>(School not yet active)</i></p>"
-  body << "<p><b>School Name:</b> #{params[:school_name]}</p>"
-  body << "<p>#{params[:school_address1]}</p>"
-  body << "<p>#{params[:school_address2]}</p>"
-  body << "<p>#{params[:school_city]} #{params[:state]} #{params[:school_zip]}</p>"
-  body << "<p><b>Contact:</b> <b>#{params[:first_name]} #{params[:last_name]}</p>"
-  body << "<p><b>Email:</b> <b>#{params[:email]}</p>"
-  body << "<p><b>Phone:</b> <b>#{params[:phone]}</p>"
-  body << "<p><b>Number of students:</b> <b>#{params[:students]}</p>"
+  body << "<h3>Educator Request for eCD Program</h3>"
+  body << "<p><b>School:</b> #{params[:school_name]}</p>"
+  body << "<p>#{params[:school_address1]} <br />#{params[:school_address2]} <br />#{params[:school_city]} #{params[:state]} #{params[:school_zip]}</p>"
+  body << "<p><b>Contact:</b> #{params[:first_name]} #{params[:last_name]}</p>"
+  body << "<p><b>Email:</b> #{params[:email]}</p>"
+  body << "<p><b>Phone:</b> #{params[:phone]}</p>"
+  body << "<p><b>Number of students:</b> #{params[:students]}</p>"
 
   Pony.mail(
     headers: { 'Content-Type' => 'text/html' },
-    to: 'skip@recountant.com',
+    to: 'info@careertrain.com, skip@recountant.com',
     from: 'no-reply@eCareerDirection.com',
     subject: 'Request eCD Program from Educator',
     body: body
